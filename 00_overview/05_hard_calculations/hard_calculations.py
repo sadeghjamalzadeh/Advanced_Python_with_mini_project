3c32d54c0583cfeede95d76bd12d52fdaf7cb30b
jamalzadehsadegh <jamalzadehsadegh@gmail.com>
2023-02-09 23:12:44 +0330

    hard calculations

---
+++ 00_overview/05_hard_calculations/hard_calculations.py
@@ -0,0 +1,6 @@
+import math
+
+x = int(input())
+a = math.ceil(math.pow(x, 5 / 3) + math.tan(math.radians(x)))
+b = math.floor(math.pow(math.pi, 2 + math.atan(math.pow(math.sin(math.radians(x)), 2))))
+print(math.gcd(a, b))
