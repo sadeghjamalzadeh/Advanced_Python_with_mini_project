4de32987d38f24265e1de5048300391b0b883084
jamalzadehsadegh <jamalzadehsadegh@gmail.com>
2023-02-12 19:55:57 +0330

    fix bug on easy_hard

--- 02_data_structures/04_easy_hard/easy_hard.py
+++ 02_data_structures/04_easy_hard/easy_hard.py
@@ -1,2 +1,1 @@
-print(*[int(value) for index, value in enumerate(list(map(int, input().split(" ")))) if
-        ((index + 1) % 6) == 0 and int(value) % 6 == 0])
+print(*sorted([int(value) for index, value in enumerate(list(map(int, input().split(" ")))) if ((index + 1) % 6) == 0 and int(value) % 6 == 0]))
